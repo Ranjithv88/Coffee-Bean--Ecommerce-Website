dd6f567eebb3754a3a84ef8bd093c345be7055e2
Ojhatrilo <ojhatrilochan5@gmail.com>
2024-11-12 14:28:31 +0530

    add BackEnd

---
+++ CoffeeBean/src/main/java/com/SpringBoot/CoffeeBean/CoffeeBeanApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.CoffeeBean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeBeanApplication.class, args);
+	}
+
+}
---
+++ CoffeeBean/src/test/java/com/SpringBoot/CoffeeBean/CoffeeBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.CoffeeBean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeBeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
